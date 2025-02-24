d21069819eb7d9838fca3860ac49ffaeea815d0b
r.shahbaz <r.shahbaz.rs73@gmail.com>
2025-02-24 16:37:05 +0330

    refactor: upload builded files

--- layers/housenumber/housenumber_centroid.sql
+++ layers/housenumber/housenumber_centroid.sql
@@ -86,14 +86,12 @@ CREATE TRIGGER trigger_store
     AFTER INSERT OR UPDATE
     ON osm_housenumber_point
     FOR EACH ROW
-    WHEN (pg_trigger_depth() < 1)
 EXECUTE PROCEDURE housenumber.store();
 
 CREATE TRIGGER trigger_flag
     AFTER INSERT OR UPDATE
     ON osm_housenumber_point
     FOR EACH STATEMENT
-    WHEN (pg_trigger_depth() < 1)
 EXECUTE PROCEDURE housenumber.flag();
 
 CREATE CONSTRAINT TRIGGER trigger_refresh
